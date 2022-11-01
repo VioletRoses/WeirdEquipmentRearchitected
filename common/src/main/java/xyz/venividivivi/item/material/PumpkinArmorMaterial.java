317b20ffa53732d1c2388f80c64e9139273086ee
Vivian Rose <VioletRose@VeniVidiVivi.xyz>
2022-10-31 19:35:41 -0500

    Added all items previously in the mod

---
+++ common/src/main/java/xyz/venividivivi/item/material/PumpkinArmorMaterial.java
@@ -0,0 +1,49 @@
+package xyz.venividivivi.item.material;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+
+public class PumpkinArmorMaterial implements ArmorMaterial {
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return 150;
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return 1;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 8;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return null;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.JACK_O_LANTERN);
+    }
+
+    @Override
+    public String getName() {
+        return "pumpkin";
+    }
+
+    @Override
+    public float getToughness() {
+        return 0;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0;
+    }
+}
--- common/src/main/java/xyz/venividivivi/registry/WeirdEquipmentItems.java
+++ common/src/main/java/xyz/venividivivi/registry/WeirdEquipmentItems.java
@@ -5,19 +5,23 @@ import dev.architectury.registry.item.ItemPropertiesRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.fabricmc.api.EnvType;
-import net.minecraft.item.ToolMaterials;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import xyz.venividivivi.WeirdEquipment;
 import xyz.venividivivi.item.*;
 import xyz.venividivivi.item.material.CactusMaterial;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.registry.Registry;
+import xyz.venividivivi.item.material.PumpkinArmorMaterial;
 
 
 public class WeirdEquipmentItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(WeirdEquipment.MOD_ID, Registry.ITEM_KEY);
 
+
+    public static final RegistrySupplier<Item> JACK_O_HELMET = ITEMS.register("jack_o_helmet", () ->
+            new JackOHelmetItem(new PumpkinArmorMaterial(), EquipmentSlot.HEAD, new Item.Settings().group(ItemGroup.COMBAT)));
 
     public static final RegistrySupplier<Item> CACTUS_SWORD = ITEMS.register("cactus_sword", () ->
             new CactusSword(new CactusMaterial(), 1, -2f, new Item.Settings().group(ItemGroup.COMBAT)));
@@ -28,14 +32,26 @@ public class WeirdEquipmentItems {
     public static final RegistrySupplier<Item> BLOCK_CANNON = ITEMS.register("block_cannon", () ->
             new BlockCannonItem(new Item.Settings().group(ItemGroup.TOOLS).maxDamage(640)));
 
+    public static final RegistrySupplier<Item> DIAMOND_SHEARS = ITEMS.register("diamond_shears", () ->
+            new ShearsItem(new Item.Settings().maxDamage(1000).group(ItemGroup.TOOLS)));
     public static final RegistrySupplier<Item> NETHERITE_TORCH_PICKAXE = ITEMS.register("netherite_torch_pickaxe", () ->
             new NetheriteTorchPickaxeItem(ToolMaterials.NETHERITE, 1, -2.8f, new Item.Settings().group(ItemGroup.TOOLS)));
+
+    public static final RegistrySupplier<Item> BOTTOMLESS_WATER_BUCKET = ITEMS.register("bottomless_water_bucket", () ->
+            new BucketItem(Fluids.WATER, new Item.Settings().group(ItemGroup.TOOLS).maxCount(1)));
+
+    public static final RegistrySupplier<Item> LARGE_LAVA_BUCKET = ITEMS.register("large_lava_bucket", () ->
+            new BucketItem(Fluids.LAVA, new Item.Settings().group(ItemGroup.TOOLS).maxDamage(9)));
 
     public static final RegistrySupplier<Item> TORCH_BOW = ITEMS.register("torch_bow", () ->
             new TorchBowItem(new Item.Settings().group(ItemGroup.TOOLS).maxDamage(384)));
 
+    public static final RegistrySupplier<Item> TORCH_CORE = ITEMS.register("torch_core", () ->
+            new Item(new Item.Settings().group(ItemGroup.MATERIALS)));
+
     public static final RegistrySupplier<Item> ROPE = ITEMS.register("rope", () ->
             new WallHangingBlockItem(WeirdEquipmentBlocks.ROPE.get(), WeirdEquipmentBlocks.WALL_ROPE.get(), new Item.Settings().group(ItemGroup.MISC)));
+
     public static final RegistrySupplier<Item> SMALL_ROPE_COIL = ITEMS.register("small_rope_coil", () ->
             new RopeCoilItem(new Item.Settings().group(ItemGroup.TOOLS).maxCount(32), 9));
     public static final RegistrySupplier<Item> MEDIUM_ROPE_COIL = ITEMS.register("medium_rope_coil", () ->
